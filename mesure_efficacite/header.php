b3c563d0ada9c3bf839ec2614c05a975889c7b47
vm-agoralogie <virginie.moore@agoralogie.fr>
2017-10-26 11:43:28 +0200

    logos partenaires dans header

---
+++ mesure_efficacite/header.php
@@ -0,0 +1,4 @@
+<header>
+	<img src="../img/logo_recolnat.png" style="height:60px"/>
+	<img src="../img/logo_dicen-idf.png" style="height:40px;padding-left: 20px;">
+</header>
--- mesure_efficacite/index_statistiques.php
+++ mesure_efficacite/index_statistiques.php
@@ -6,6 +6,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 <body>
+<?php include("header.php");?>
 <?php
 	
 	echo '<fieldset>'."\n";
