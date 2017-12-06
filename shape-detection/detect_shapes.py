e72e609035c43da0132f1682316047f66d90857a
pmlaroche-agoralogie <philippe.laroche@agoralogie.fr>
2017-12-06 23:28:45 +0100

    bricolage

--- shape-detection/detect_shapes.py
+++ shape-detection/detect_shapes.py
@@ -16,7 +16,7 @@ args = vars(ap.parse_args())
 # load the image and resize it to a smaller factor so that
 # the shapes can be approximated better
 image = cv2.imread(args["image"])
-resized = imutils.resize(image, width=300)
+resized = imutils.resize(image, width=700)
 ratio = image.shape[0] / float(resized.shape[0])
 
 # convert the resized image to grayscale, blur it slightly,
@@ -24,6 +24,10 @@ ratio = image.shape[0] / float(resized.shape[0])
 gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+
+edges = cv2.Canny(gray,30,100)
+thresh = cv2.Canny(gray,30,100)
+cv2.imwrite("tt.jpg",edges)
 
 # find contours in the thresholded image and initialize the
 # shape detector
@@ -37,8 +41,11 @@ for c in cnts:
 	# compute the center of the contour, then detect the name of the
 	# shape using only the contour
 	M = cv2.moments(c)
-	cX = int((M["m10"] / M["m00"]) * ratio)
-	cY = int((M["m01"] / M["m00"]) * ratio)
+	cX = 1
+	cY = 1
+	if  M["m00"] > 0:
+		cX = int((M["m10"] / M["m00"]) * ratio)
+		cY = int((M["m01"] / M["m00"]) * ratio)
 	shape = sd.detect(c)
 
 	# multiply the contour (x, y)-coordinates by the resize ratio,
@@ -46,10 +53,14 @@ for c in cnts:
 	c = c.astype("float")
 	c *= ratio
 	c = c.astype("int")
-	cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-	cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-		0.5, (255, 255, 255), 2)
+	if shape == "rectangle": 
+		cv2.drawContours(image, [c], -1, (0, 255, 0),1,-1)
+		cv2.putText(image, shape + str(M["m10"]), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+			0.5, (5, 5, 255), 2)
 
 	# show the output image
-	cv2.imshow("Image", image)
-	cv2.waitKey(0)
+        print shape 
+
+#	cv2.imshow("Image", image)
+#	cv2.waitKey(0)
+cv2.imwrite("zz.jpg",image);
